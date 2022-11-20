c73a494ee267632280beefa4d26a1dc48d75c525
Woopertail <woopertail@gmail.com>
2022-11-20 16:23:33 +0300

    🔧 Добавлен загрузчик конфигов Utils/config_loader.py Создана демонстрационная база данных Storage/products/data_bases/example.db Изменен auto_delivery.cfg

---
+++ Utils/config_loader.py
@@ -0,0 +1,192 @@
+import configparser
+import codecs
+import os
+import sqlite3
+import json
+
+
+def check_param(param_name: str, obj: dict | configparser.SectionProxy,
+                valid_values: list[str, None] | None = None,
+                raise_ex_if_not_exists: bool = True) -> str | None:
+    """
+    Проверяет, существует ли в переданном словаре указанный ключ, и если да, валидно ли его значение (перед проверкой
+    значение проходит через .strip()).
+    Если нет - райзит исключение.
+
+    :param param_name: название ключа.
+    :param obj: словарь.
+    :param valid_values: список валидных значений. Если не указан, любая непустая строка - валидное значение.
+    :param raise_ex_if_not_exists: райзить ли исключение, если параметр не обнаружен
+    (если параметр опционален, например).
+    :return: Значение ключа, если ключ найден и его значение валидно. Если ключ не найден и
+    raise_ex_if_not_exists = False - возвращает None. В любом другом случае райзит исключения.
+    """
+    value = obj.get(param_name)
+    if value is None:
+        if raise_ex_if_not_exists:
+            raise Exception  # todo: создать кастомное исключение: отсутсвует параметр.
+        return value
+
+    value = value.strip()
+    if not value:
+        raise Exception  # todo: создать кастомное исключение: параметр пуст.
+
+    if valid_values is not None:
+        if value not in valid_values:
+            raise Exception  # todo: создать кастомное исключение: значение параметра не в списке допустимых.
+
+    return value
+
+
+def check_db_table_exists(db_path: str, table_name: str) -> bool:
+    """
+    Проверяет существует ли таблица в файле базы данных.
+
+    :param db_path: путь до файла базы данных.
+    :param table_name: название таблицы.
+    :return: True, если указанная таблица существует, False - если нет.
+    """
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute("SELECT * FROM sqlite_master WHERE type = 'table'")
+    result = c.fetchall()
+    c.close()
+    conn.close()
+
+    table_names = [table[1] for table in result]
+    return table_name in table_names
+
+
+def check_table_column_exists(db_path: str, table_name: str, column_name: str) -> bool:
+    """
+    Проверяет существует ли столбец в таблице базы данных.
+
+    :param db_path: путь до файла базы данных.
+    :param table_name: название таблицы.
+    :param column_name: название столбца.
+    :return: True, если указанный столбец существует, False - если нет.
+    """
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+
+    c.execute(f"PRAGMA table_info({table_name})")
+    result = c.fetchall()
+    c.close()
+    conn.close()
+
+    column_names = [column[1] for column in result]
+    return column_name in column_names
+
+
+def get_db_entries_count(db_path: str, table_name: str) -> int:
+    """
+    Возвращает количество записей в таблице table_name.
+    :param db_path: путь до файла базы данных.
+    :param table_name: название таблицы.
+    :return: количество записей в таблице table_name
+    """
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute(f"""SELECT COUNT(*) FROM {table_name}""")
+    result = c.fetchall()[0][0]
+    c.close()
+    conn.close()
+    return result
+
+
+def load_main_config(config_path: str) -> configparser.ConfigParser:
+    config = configparser.ConfigParser()
+    config.read_file(codecs.open(config_path, "r", "utf8"))
+    return config
+
+
+def load_lots_config(config_path: str) -> configparser.ConfigParser:
+    """
+    Парсит и проверяет на правильность конфиг с лотами.
+
+    :param config_path: путь до файла с лотами.
+    :return: спарсеный конфиг лотов.
+    """
+    config = configparser.ConfigParser()
+    config.read_file(codecs.open(config_path, "r", "utf8"))
+    for alias in config.sections():
+        check_param("name", config[alias])
+        check_param("exclude_auto_restore", config[alias], valid_values=["0", "1"], raise_ex_if_not_exists=False)
+    return config
+
+
+def load_auto_response_config(config_path: str) -> configparser.ConfigParser:
+    """
+    Парсит и проверяет на правильность конфиг с командами.
+
+    :param config_path: путь до файла с лотами.
+    :return: спарсеный конфиг команд.
+    """
+    config = configparser.ConfigParser()
+    config.read_file(codecs.open(config_path, "r", "utf8"))
+    for command in config.sections():
+        check_param("response", config[command])
+    return config
+
+
+def load_auto_delivery_config(config_path: str) -> configparser.ConfigParser:
+    """
+    Парсит и проверяет на правильность конфиг с товарами для авто-выдачи.
+
+    :param config_path: путь до файла с лотами.
+    :return: спарсеный конфиг товаров для авто-выдачи.
+    :param config_path:
+    :return:
+    """
+    config = configparser.ConfigParser()
+    config.read_file(codecs.open(config_path, "r", "utf8"))
+
+    for lot in config.sections():
+        # Проверяем обязательный параметр response.
+        lot_response = check_param("response", config[lot])
+
+        # Проверяем указан параметр products_storage_type и если да, то правильно ли.
+        products_storage_type = check_param("products_storage_type", config[lot], raise_ex_if_not_exists=False)
+        if products_storage_type is None:
+            # Если данного параметра нет, то в текущем лоте более нечего проверять -> переход на след. итерацию.
+            continue
+
+        # Проверяем, есть ли хотя бы 1 переменная $product в тексте response.
+        if "$product" not in lot_response:
+            raise Exception  # todo: создать и добавить кастомное исключение: отсутствует переменная в тексте.
+
+        # Проверяем указан ли параметр products_file_name и существует ли файл, указанный в этом параметре.
+        products_file_path = check_param("products_file_path", config[lot])
+        if not os.path.exists(products_file_path):
+            raise Exception  # todo: создать и добавить кастомное исключение: файл с товарами отсутствует.
+
+        # Проверяем валидность данных в файле, указанном в products_file_name
+        if products_storage_type == "json":
+            with open(products_file_path, "r", encoding="utf-8") as f:
+                products = f.read()
+            try:
+                products = json.loads(products)
+            except json.decoder.JSONDecodeError:
+                raise Exception  # todo: создать и добавить кастомное исключение: невалидный формат данных.
+            # Проверяем наличие товара
+            if len(products) < 1:
+                raise Exception  # todo: создать и добавить кастомное исключение: кол-во товара = 0
+
+        elif products_storage_type == "db":
+            # Проверяем указан ли параметр db_table_name и существует ли таблица с именем, указанным в этом параметре.
+            db_table_name = check_param("db_table_name", config[lot])
+            check = check_db_table_exists(products_file_path, db_table_name)
+            if not check:
+                raise Exception  # todo: создать и добавить кастомное исключение: таблицы не существует.
+
+            # Проверяем указан ли параметр db_column_name и
+            # существует ли столбец с именем, указанным в этом параметре, в таблице db_table_name.
+            db_column_name = check_param("db_column_name", config[lot])
+            check = check_table_column_exists(products_file_path, db_table_name, db_column_name)
+            if not check:
+                raise Exception  # todo: создать и добавить кастомное исключение: столбца не существует.
+
+            if get_db_entries_count(products_file_path, db_table_name) < 1:
+                raise Exception  # todo: создать и добавить кастомное исключение: кол-во товара = 0
+
+    return config
--- main.py
+++ main.py
@@ -1,5 +1,46 @@
-import configparser
-import codecs
+import os
+import sys
+import colorama
 
-config = configparser.ConfigParser()
-config.read_file(codecs.open("configs/_main.cfg"))
+import Utils.config_loader as cfg_loader
+from Utils.logger import Logger
+
+
+def get_abs_path(path: str) -> str:
+    """
+    Возвращает полный путь. Проверяет, запущенно ли приложение как .exe файл или как python скрипт.
+    :param path: путь до папки/файла.
+    :return: Полный путь.
+    """
+    if getattr(sys, 'frozen', False):
+        return os.path.join(os.path.dirname(sys.executable), path)
+    else:
+        return os.path.join(os.path.dirname(__file__), path)
+
+
+colorama.init()
+logger = Logger()
+
+try:
+    main_config_path = get_abs_path("configs/_main.cfg")
+    MAIN_CONFIG = cfg_loader.load_main_config(main_config_path)
+except Exception:
+    pass  # todo: сделать обработку разных исключений.
+
+try:
+    lots_config_path = get_abs_path("configs/lots.cfg")
+    LOTS_CONFIG = cfg_loader.load_lots_config(lots_config_path)
+except Exception:
+    pass  # todo: сделать обработку разных исключений.
+
+try:
+    auto_response_config_path = get_abs_path("configs/auto_response.cfg")
+    AUTO_RESPONSE_CONFIG = cfg_loader.load_auto_response_config(auto_response_config_path)
+except Exception:
+    pass  # todo: сделать обработку разных исключений.
+
+try:
+    auto_delivery_config_path = get_abs_path("configs/auto_delivery.cfg")
+    AUTO_DELIVERY_CONFIG = cfg_loader.load_auto_response_config(auto_delivery_config_path)
+except Exception:
+    pass  # todo: сделать обработку разных исключений.
