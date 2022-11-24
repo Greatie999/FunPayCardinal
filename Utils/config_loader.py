62f5c18b9f9be003003b7b008bfc2b05b67a3cd2
Woopertail <woopertail@gmail.com>
2022-11-24 21:55:42 +0300

    🗑️ Убрана поддержка баз данных для хранения товаров для авто-выдачи.

--- Utils/config_loader.py
+++ Utils/config_loader.py
@@ -1,12 +1,11 @@
 import configparser
 import codecs
 import os
-import sqlite3
 import json
 
-from .exceptions import (ParamNotExists, ParamValueEmpty, ParamValueNotValid,
-                         NoSuchTableError, NoSuchColumnError, NoProductsError, NoProductVarError,
-                         NoSuchProductFileError, JSONParseError)
+from Utils.exceptions import (ParamNotExists, ParamValueEmpty, ParamValueNotValid,
+                              NoSuchTableError, NoSuchColumnError, NoProductsError, NoProductVarError,
+                              NoSuchProductFileError, JSONParseError)
 
 
 def check_param(param_name: str,
@@ -52,62 +51,6 @@ def check_param(param_name: str,
     return value
 
 
-def check_db_table_exists(db_path: str, table_name: str) -> bool:
-    """
-    Проверяет существует ли таблица в файле базы данных.
-
-    :param db_path: путь до файла базы данных.
-    :param table_name: название таблицы.
-    :return: True, если указанная таблица существует, False - если нет.
-    """
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
-    c.execute("SELECT * FROM sqlite_master WHERE type = 'table'")
-    result = c.fetchall()
-    c.close()
-    conn.close()
-
-    table_names = [table[1] for table in result]
-    return table_name in table_names
-
-
-def check_table_column_exists(db_path: str, table_name: str, column_name: str) -> bool:
-    """
-    Проверяет существует ли столбец в таблице базы данных.
-
-    :param db_path: путь до файла базы данных.
-    :param table_name: название таблицы.
-    :param column_name: название столбца.
-    :return: True, если указанный столбец существует, False - если нет.
-    """
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
-
-    c.execute(f"PRAGMA table_info({table_name})")
-    result = c.fetchall()
-    c.close()
-    conn.close()
-
-    column_names = [column[1] for column in result]
-    return column_name in column_names
-
-
-def get_db_entries_count(db_path: str, table_name: str) -> int:
-    """
-    Возвращает количество записей в таблице table_name.
-    :param db_path: путь до файла базы данных.
-    :param table_name: название таблицы.
-    :return: количество записей в таблице table_name
-    """
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
-    c.execute(f"""SELECT COUNT(*) FROM {table_name}""")
-    result = c.fetchall()[0][0]
-    c.close()
-    conn.close()
-    return result
-
-
 def load_main_config(config_path: str) -> configparser.ConfigParser:
     config = configparser.ConfigParser()
     config.read_file(codecs.open(config_path, "r", "utf8"))
@@ -123,10 +66,9 @@ def load_lots_config(config_path: str) -> configparser.ConfigParser:
     """
     config = configparser.ConfigParser()
     config.read_file(codecs.open(config_path, "r", "utf8"))
-    for alias in config.sections():
-        check_param("name", alias, "lots.cfg", config[alias])
-        check_param("exclude_auto_restore", alias, "lots.cfg",
-                    config[alias], valid_values=["0", "1"], raise_ex_if_not_exists=False)
+    for lot in config.sections():
+        check_param("exclude_auto_restore", lot, "lots.cfg",
+                    config[lot], valid_values=["0", "1"], raise_ex_if_not_exists=False)
     return config
 
 
@@ -158,49 +100,31 @@ def load_auto_delivery_config(config_path: str) -> configparser.ConfigParser:
         # Проверяем обязательный параметр response.
         lot_response = check_param("response", lot, "auto_delivery.cfg", config[lot])
 
-        # Проверяем указан параметр products_storage_type и если да, то правильно ли.
-        products_storage_type = check_param("products_storage_type", lot, "auto_delivery.cfg",
-                                            config[lot], raise_ex_if_not_exists=False)
-        if products_storage_type is None:
+        # Проверяем указан параметр productsFilePath.
+        products_file_path = check_param("productsFilePath", lot, "auto_delivery.cfg",
+                                         config[lot], raise_ex_if_not_exists=False)
+        if products_file_path is None:
             # Если данного параметра нет, то в текущем лоте более нечего проверять -> переход на след. итерацию.
             continue
+
+        # Проверяем, существует ли файл.
+        if not os.path.exists(products_file_path):
+            raise NoSuchProductFileError(lot, products_file_path)
+
+        # Проверяем валидность json'а.
+        with open(products_file_path, "r", encoding="utf-8") as f:
+            products = f.read()
+        try:
+            products = json.loads(products)
+        except json.decoder.JSONDecodeError as e:
+            raise JSONParseError(lot, products_file_path, str(e))
+
+        # Проверяем кол-во товара
+        if len(products) < 1:
+            raise NoProductsError(products_file_path)
 
         # Проверяем, есть ли хотя бы 1 переменная $product в тексте response.
         if "$product" not in lot_response:
             raise NoProductVarError(lot)
-
-        # Проверяем указан ли параметр products_file_name и существует ли файл, указанный в этом параметре.
-        products_file_path = check_param("products_file_path", lot, "auto_delivery.cfg", config[lot])
-        if not os.path.exists(products_file_path):
-            raise NoSuchProductFileError(lot, products_file_path)
-
-        # Проверяем валидность данных в файле, указанном в products_file_name
-        if products_storage_type == "json":
-            with open(products_file_path, "r", encoding="utf-8") as f:
-                products = f.read()
-            try:
-                products = json.loads(products)
-            except json.decoder.JSONDecodeError as e:
-                raise JSONParseError(lot, products_file_path, str(e))
-            # Проверяем наличие товара
-            if len(products) < 1:
-                raise NoProductsError(products_file_path)
-
-        elif products_storage_type == "db":
-            # Проверяем указан ли параметр db_table_name и существует ли таблица с именем, указанным в этом параметре.
-            db_table_name = check_param("db_table_name", lot, "auto_delivery.cfg", config[lot])
-            check = check_db_table_exists(products_file_path, db_table_name)
-            if not check:
-                raise NoSuchTableError(products_file_path, db_table_name)
-
-            # Проверяем указан ли параметр db_column_name и
-            # существует ли столбец с именем, указанным в этом параметре, в таблице db_table_name.
-            db_column_name = check_param("db_column_name", lot, "auto_delivery.cfg", config[lot])
-            check = check_table_column_exists(products_file_path, db_table_name, db_column_name)
-            if not check:
-                raise NoSuchColumnError(products_file_path, db_table_name, db_column_name)
-
-            if get_db_entries_count(products_file_path, db_table_name) < 1:
-                raise NoProductsError(products_file_path)
 
     return config
